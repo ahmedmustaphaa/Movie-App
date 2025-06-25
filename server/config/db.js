8318ed1415a0baaea1b0edfc3882aadab595cf63
ahmed mostafa <ahmed mistafasbshhwhwh@gmail.com>
2025-06-25 07:12:20 +0300

    all data recieved

--- server/config/db.js
+++ server/config/db.js
@@ -8,6 +8,7 @@ const connectedDb = async () => {
       useUnifiedTopology: true,
     });
     console.log("✅ Connected to MongoDB");
+    console.log("URI:", process.env.MONGODB_URI); // ✅ لازم تطبع الرابط
   } catch (error) {
     console.error("❌ MongoDB connection error:", error.message);
     process.exit(1); // خروج من التطبيق في حالة الفشل
