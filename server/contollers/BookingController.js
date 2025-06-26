ade4fb9fa9aec93e362fd23cfcc25d8fb55c6e65
ahmed mostafa <ahmed mistafasbshhwhwh@gmail.com>
2025-06-26 18:29:04 +0300

    Fix module case sensitivity for user.js

--- server/contollers/BookingController.js
+++ server/contollers/BookingController.js
@@ -1,112 +1,112 @@
-import { Show } from "../models/Show.js";
-import { Booking } from "../models/Booking.js";
-import Stripe from 'stripe';
+// import { Show } from "../models/Show.js";
+// import { Booking } from "../models/Booking.js";
+// import Stripe from 'stripe';
 
- const checkSeatAvailability = async (showId, selectedSeats) => {
-  try {
-    const showData = await Show.findById(showId);
-    if (!showData) return false;
+//  const checkSeatAvailability = async (showId, selectedSeats) => {
+//   try {
+//     const showData = await Show.findById(showId);
+//     if (!showData) return false;
 
-    const occupiedSeats = showData.occupiedSeats;
+//     const occupiedSeats = showData.occupiedSeats;
 
-    // تحقق: هل أي كرسي مختار محجوز؟
-    const isAnySeatsTaken = selectedSeats.some(seat => seat in occupiedSeats);
+//     // تحقق: هل أي كرسي مختار محجوز؟
+//     const isAnySeatsTaken = selectedSeats.some(seat => seat in occupiedSeats);
 
-    return !isAnySeatsTaken; // لو ولا واحد محجوز => متاح ✅
+//     return !isAnySeatsTaken; // لو ولا واحد محجوز => متاح ✅
 
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
-};
+//   } catch (error) {
+//     console.log(error);
+//     return false;
+//   }
+// };
 
-export const createBooking = async (req, res) => {
-  try {
-    const { userId } = await req.auth(); 
-    const { showId, selectedSeats } = req.body;
-    const { origin } = req.headers;
+// export const createBooking = async (req, res) => {
+//   try {
+//     const { userId } = await req.auth(); 
+//     const { showId, selectedSeats } = req.body;
+//     const { origin } = req.headers;
 
-    const isAvailable = await checkSeatAvailability(showId, selectedSeats);
-    if (!isAvailable) {
-      return res.status(400).json({
-        success: false,
-        message: "الكراسي المحددة محجوزة بالفعل"
-      });
-    }
+//     const isAvailable = await checkSeatAvailability(showId, selectedSeats);
+//     if (!isAvailable) {
+//       return res.status(400).json({
+//         success: false,
+//         message: "الكراسي المحددة محجوزة بالفعل"
+//       });
+//     }
 
-    const showData = await Show.findById(showId).populate("movie");
-    const totalAmount = showData.showPrice * selectedSeats.length;
+//     const showData = await Show.findById(showId).populate("movie");
+//     const totalAmount = showData.showPrice * selectedSeats.length;
 
-    const booking = await Booking.create({
-      user: userId,
-      show: showId,
-      amount: totalAmount,
-      bookedSeats: selectedSeats
-    });
+//     const booking = await Booking.create({
+//       user: userId,
+//       show: showId,
+//       amount: totalAmount,
+//       bookedSeats: selectedSeats
+//     });
 
-    selectedSeats.forEach(seat => {
-      showData.occupiedSeats[seat] = userId;
-    });
+//     selectedSeats.forEach(seat => {
+//       showData.occupiedSeats[seat] = userId;
+//     });
 
-    showData.markModified("occupiedSeats");
-    await showData.save();
+//     showData.markModified("occupiedSeats");
+//     await showData.save();
 
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2023-08-16'
-    });
+//     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+//       apiVersion: '2023-08-16'
+//     });
 
-    const session = await stripe.checkout.sessions.create({
-      success_url: `${origin}/loading/my-bookings`,
-      cancel_url: `${origin}/my-bookings`,
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: showData.movie.title,
-            },
-            unit_amount: Math.floor(booking.amount * 100),
-          },
-          quantity: 1,
-        },
-      ],
-      mode: 'payment',
-      metadata: {
-        bookingId: booking._id.toString(),
-      },
-      expires_at: Math.floor(Date.now() / 1000) + 30 * 60,
-    });
+//     const session = await stripe.checkout.sessions.create({
+//       success_url: `${origin}/loading/my-bookings`,
+//       cancel_url: `${origin}/my-bookings`,
+//       line_items: [
+//         {
+//           price_data: {
+//             currency: 'usd',
+//             product_data: {
+//               name: showData.movie.title,
+//             },
+//             unit_amount: Math.floor(booking.amount * 100),
+//           },
+//           quantity: 1,
+//         },
+//       ],
+//       mode: 'payment',
+//       metadata: {
+//         bookingId: booking._id.toString(),
+//       },
+//       expires_at: Math.floor(Date.now() / 1000) + 30 * 60,
+//     });
 
-    booking.paymentLink = session.url;
-    await booking.save();
+//     booking.paymentLink = session.url;
+//     await booking.save();
 
-    return res.status(201).json({
-      success: true,
-      url: session.url,
-    });
+//     return res.status(201).json({
+//       success: true,
+//       url: session.url,
+//     });
 
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-export const getOccupiedSeats = async (req, res) => {
-  try {
-    const { showId } = req.params; // استخراج showId من الرابط
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json({ success: false, message: error.message });
+//   }
+// };
+// export const getOccupiedSeats = async (req, res) => {
+//   try {
+//     const { showId } = req.params; // استخراج showId من الرابط
 
-    const showData = await Show.findById(showId);
-    if (!showData) {
-      return res.status(404).json({ success: false, message: "العرض غير موجود" });
-    }
+//     const showData = await Show.findById(showId);
+//     if (!showData) {
+//       return res.status(404).json({ success: false, message: "العرض غير موجود" });
+//     }
 
-    // رجّع الكراسي المحجوزة
-    return res.status(200).json({
-      success: true,
-      occupiedSeats: showData.occupiedSeats
-    });
+//     // رجّع الكراسي المحجوزة
+//     return res.status(200).json({
+//       success: true,
+//       occupiedSeats: showData.occupiedSeats
+//     });
 
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, message: error.message });
-  }
-}
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json({ success: false, message: error.message });
+//   }
+// }
--- server/contollers/showContoller.js
+++ server/contollers/showContoller.js
@@ -1,138 +1,138 @@
-import axios from "axios"
+// import axios from "axios"
 
-import { Movie } from "../models/movie.js";
-import { Show } from "../models/Show.js";
-import { set } from "mongoose";
+// import { Movie } from "../models/movie.js";
+// import { Show } from "../models/Show.js";
+// import { set } from "mongoose";
 
-export const getNowPlayingMovie=async(req,res)=>{
-  try{
-          const {data}=await axios.get('https://api.themoviedb.org/3/movie/now_playing',{
-            headers:{
-                 Authorization: `Bearer ${process.env.TMDB_API_KEY}`
-            }
-          })
+// export const getNowPlayingMovie=async(req,res)=>{
+//   try{
+//           const {data}=await axios.get('https://api.themoviedb.org/3/movie/now_playing',{
+//             headers:{
+//                  Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+//             }
+//           })
 
-          const movies=data.results;
-          res.json({success:true,movies:movies})
-  }catch(error){
-       res.json({success:false,message:error.message})
-  }
-}
+//           const movies=data.results;
+//           res.json({success:true,movies:movies})
+//   }catch(error){
+//        res.json({success:false,message:error.message})
+//   }
+// }
 
-export const addShow = async (req, res) => {
-  try {
-    const { movieId, showsInput, showPrice } = req.body;
+// export const addShow = async (req, res) => {
+//   try {
+//     const { movieId, showsInput, showPrice } = req.body;
 
-    // تحقق من الحقول الأساسية
-    if (!movieId || !Array.isArray(showsInput) || typeof showPrice !== 'number') {
-      return res.status(400).json({
-        success: false,
-        message: "بيانات غير صالحة. تأكد من إرسال movieId, showsInput, showPrice"
-      });
-    }
+//     // تحقق من الحقول الأساسية
+//     if (!movieId || !Array.isArray(showsInput) || typeof showPrice !== 'number') {
+//       return res.status(400).json({
+//         success: false,
+//         message: "بيانات غير صالحة. تأكد من إرسال movieId, showsInput, showPrice"
+//       });
+//     }
 
-    // التحقق من وجود الفيلم في قاعدة البيانات
-    let movie = await Movie.findById(movieId);
+//     // التحقق من وجود الفيلم في قاعدة البيانات
+//     let movie = await Movie.findById(movieId);
 
-    // إذا لم يكن الفيلم موجودًا، يتم جلبه من TMDB وحفظه
-    if (!movie) {
-      const [movieDetailsResponse, movieCreditsResponse] = await Promise.all([
-        axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
-          headers: {
-            Authorization: `Bearer ${process.env.TMDB_API_KEY}`
-          }
-        }),
-        axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits`, {
-          headers: {
-            Authorization: `Bearer ${process.env.TMDB_API_KEY}`
-          }
-        })
-      ]);
+//     // إذا لم يكن الفيلم موجودًا، يتم جلبه من TMDB وحفظه
+//     if (!movie) {
+//       const [movieDetailsResponse, movieCreditsResponse] = await Promise.all([
+//         axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
+//           headers: {
+//             Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+//           }
+//         }),
+//         axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits`, {
+//           headers: {
+//             Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+//           }
+//         })
+//       ]);
 
-      const movieApiData = movieDetailsResponse.data;
-      const movieCreditsData = movieCreditsResponse.data;
+//       const movieApiData = movieDetailsResponse.data;
+//       const movieCreditsData = movieCreditsResponse.data;
 
-      const newMovie = await Movie.create({
-        _id: movieApiData.id,
-        title: movieApiData.title,
-        overview: movieApiData.overview,
-        poster_path: movieApiData.poster_path,
-        backdrop_path: movieApiData.backdrop_path,
-        release_date: movieApiData.release_date,
-        original_language: movieApiData.original_language,
-        tagline: movieApiData.tagline,
-        genres: movieApiData.genres.map(g => g.name),
-        casts: movieCreditsData.cast.slice(0, 5).map(c => c.name),
-        vote_average: movieApiData.vote_average,
-        runtime: movieApiData.runtime
-      });
+//       const newMovie = await Movie.create({
+//         _id: movieApiData.id,
+//         title: movieApiData.title,
+//         overview: movieApiData.overview,
+//         poster_path: movieApiData.poster_path,
+//         backdrop_path: movieApiData.backdrop_path,
+//         release_date: movieApiData.release_date,
+//         original_language: movieApiData.original_language,
+//         tagline: movieApiData.tagline,
+//         genres: movieApiData.genres.map(g => g.name),
+//         casts: movieCreditsData.cast.slice(0, 5).map(c => c.name),
+//         vote_average: movieApiData.vote_average,
+//         runtime: movieApiData.runtime
+//       });
 
-      movie = newMovie;
-    }
+//       movie = newMovie;
+//     }
 
-    // بناء العروض لإضافتها
-    const showsToCreate = [];
+//     // بناء العروض لإضافتها
+//     const showsToCreate = [];
 
-    showsInput.forEach(show => {
-      const showDate = show.date;
+//     showsInput.forEach(show => {
+//       const showDate = show.date;
 
-      // ✅ استخدم show.times بدل show.time
-      if (Array.isArray(show.times)) {
-        show.times.forEach(time => {
-          const dateTimeString = `${showDate}T${time}`; // مثال: 2025-06-21T18:00
-          showsToCreate.push({
-            movie: movieId,
-            showDateTime: new Date(dateTimeString),
-            showPrice,
-            occupiedSeats: []
-          });
-        });
-      }
-    });
+//       // ✅ استخدم show.times بدل show.time
+//       if (Array.isArray(show.times)) {
+//         show.times.forEach(time => {
+//           const dateTimeString = `${showDate}T${time}`; // مثال: 2025-06-21T18:00
+//           showsToCreate.push({
+//             movie: movieId,
+//             showDateTime: new Date(dateTimeString),
+//             showPrice,
+//             occupiedSeats: []
+//           });
+//         });
+//       }
+//     });
 
-    // حفظ العروض في قاعدة البيانات
-    if (showsToCreate.length > 0) {
-      await Show.insertMany(showsToCreate);
-      return res.status(201).json({
-        success: true,
-        message: "تم إضافة العروض بنجاح",
-        count: showsToCreate.length
-      });
-    } else {
-      return res.status(400).json({
-        success: false,
-        message: "لا توجد عروض لإضافتها"
-      });
-    }
+//     // حفظ العروض في قاعدة البيانات
+//     if (showsToCreate.length > 0) {
+//       await Show.insertMany(showsToCreate);
+//       return res.status(201).json({
+//         success: true,
+//         message: "تم إضافة العروض بنجاح",
+//         count: showsToCreate.length
+//       });
+//     } else {
+//       return res.status(400).json({
+//         success: false,
+//         message: "لا توجد عروض لإضافتها"
+//       });
+//     }
 
-  } catch (error) {
-    console.error("addShow error:", error);
-    return res.status(500).json({
-      success: false,
-      message: "حدث خطأ في السيرفر",
-      error: error.message
-    });
-  }
-};
-export const getShows = async (req, res) => {
-  try {
-    const shows = await Show.find({
-      showDateTime: { $gte: new Date() }
-    }).populate('movie').sort({ showDateTime: 1 });
+//   } catch (error) {
+//     console.error("addShow error:", error);
+//     return res.status(500).json({
+//       success: false,
+//       message: "حدث خطأ في السيرفر",
+//       error: error.message
+//     });
+//   }
+// };
+// export const getShows = async (req, res) => {
+//   try {
+//     const shows = await Show.find({
+//       showDateTime: { $gte: new Date() }
+//     }).populate('movie').sort({ showDateTime: 1 });
 
-    const uniqueShow = new Set(shows.map((show) => show.movie));
+//     const uniqueShow = new Set(shows.map((show) => show.movie));
 
-    res.json({
-      success: true,
-      shows: Array.from(uniqueShow)
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      success: false,
-      message: error.message
-    });
-  }
-};
+//     res.json({
+//       success: true,
+//       shows: Array.from(uniqueShow)
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({
+//       success: false,
+//       message: error.message
+//     });
+//   }
+// };
 
 
