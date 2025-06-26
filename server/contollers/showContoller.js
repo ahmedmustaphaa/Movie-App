2cfdd828f9f84536e7f5a57416e3ebfeab6fa44a
ahmed mostafa <ahmed mistafasbshhwhwh@gmail.com>
2025-06-26 19:21:51 +0300

    Fix module case sensitivity for user.js

--- server/contollers/showContoller.js
+++ server/contollers/showContoller.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-import { Movie } from "../models/movie.js";
+import { Movie } from "../models/Movie.js";
 import { Show } from "../models/Show.js";
 import { set } from "mongoose";
 
